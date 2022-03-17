d32a72b8b05a12934df2564bd5d22591a4d7d2cd
kingsmandralph <ekeneumegakwe@gmail.com>
2022-03-17 15:37:02 -0700

    This commit truly is

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: is the tested character
+ * Return: 1 if c is uppercase else 0
+ */
+
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
