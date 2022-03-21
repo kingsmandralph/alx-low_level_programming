beecceebb00f9784bac01d7641f3016c58caca6c
kingsmandralph <ekeneumegakwe@gmail.com>
2022-03-21 06:01:20 -0700

    The swap made easy

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - swap integers
+ * @a: first parameter
+ * @b: second parameter
+ * Description: swaps the values of two integers
+ * Return: Always (0);
+ */
+
+void swap_int(int *a, int *b)
+{
+*a = 42;
+*b = 98;
+}
