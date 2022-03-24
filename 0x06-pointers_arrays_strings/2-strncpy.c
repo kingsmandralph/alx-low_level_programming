3b7d67777c9fa3eb806f869d037ee03b84ec9bfa
kingsmandralph <ekeneumegakwe@gmail.com>
2022-03-24 13:57:40 -0700

    The commit now

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,10 +12,20 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int i;
-for (i = 0; i < n && src[i] != '\0'; i++)
-dest[i] = src[i];
-for ( ; i < n; i++)
-dest[i] = '\0';
-return dest;
 
+i = 0;
+
+while (src[i] != '\0' && i < n)
+{
+dest[i] = src[i];
+i++;
+}
+
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
+
+return (dest);
 }
